212ced92b4c29c4496fb86e9dee9cc36ac1e9dcb
ivin stephen <ivinstephen001@gmail.com>
2022-04-01 02:53:25 +0530

    added static file

--- {{ cookiecutter.repo_name }}/src/config/settings.py
+++ {{ cookiecutter.repo_name }}/src/config/settings.py
@@ -112,14 +112,21 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# User uploaded files (Images, Videos, PDF)
+# https://docs.djangoproject.com/en/4.0/topics/files/
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
